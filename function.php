c3d8a7514249f86b7e7e47bad7f85d187b7dfb1a
Rafii1205 <alfiansyahr@gmail.com>
2026-07-02 20:59:31 +0700

    feat: tambahdata

--- function.php
+++ function.php
@@ -1,5 +1,6 @@
 <?php
  $konek=mysqli_connect('localhost','root','','mraweekly');
+
  function tampildata($query)
  {
     global $konek; 
@@ -16,6 +17,19 @@
     
     $query = "DELETE FROM mahasiswa WHERE id= $id ";
     mysqli_query($konek, $query);
+    return mysqli_affected_rows($konek);
+ }
+ function tambah($data){
+    global $konek;
+    $nama=htmlspecialchars($data["nama"]);
+    $nim=htmlspecialchars($data["nim"]);
+    $prodi=htmlspecialchars($data["prodi"]);
+    $email=htmlspecialchars($data["email"]);
+    $no_hp=htmlspecialchars($data["no_hp"]);
+    $foto=htmlspecialchars($data["foto"]);
+
+    $query="INSERT INTO mahasiswa(nama,nim,prodi,email,no_hp,foto)values ('$nama','$nim','$prodi','$email','$no_hp','$foto')";
+    mysqli_query($konek,$query);
     return mysqli_affected_rows($konek);
  }
 ?>
